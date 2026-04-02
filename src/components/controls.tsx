0a4b0e0f175d45295f532365983a6504b996df11
carlos-dubon <holacarlosdubon@gmail.com>
2026-04-02 09:24:11 -0600

    feat: add pica

--- src/components/controls.tsx
+++ src/components/controls.tsx
@@ -1,13 +1,29 @@
 import { useAtom } from "jotai"
 import { Button } from "./ui/button"
 import { fileUploadStateAtom } from "@/state/file-upload-state"
+import pica from "pica"
+import { useCallback, useState } from "react"
 
 export const Controls = () => {
   const [state] = useAtom(fileUploadStateAtom)
+  const isDisabled = state.files.length === 0
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  const handleCompress = useCallback(async () => {
+    setIsLoading(true)
+    await new Promise((resolve) => setTimeout(resolve, 1000))
+    setIsLoading(false)
+  }, [])
+
   return (
     <div className="items-center rounded-2xl border border-border bg-card p-4">
-      {state.files.length} files
-      <Button size="lg" className="w-full">
+      <Button
+        size="lg"
+        className="w-full"
+        disabled={isDisabled}
+        onClick={handleCompress}
+      >
         Compress images
       </Button>
     </div>
